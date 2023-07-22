715d9dcc4c21ceffb19167607d9d846c880a8f40
Dylan Storey <dylanbstorey@users.noreply.github.com>
2023-07-22 00:18:46 -0400

    Update init.py

--- {{ folder_variable }}/.angreal/init.py
+++ {{ folder_variable }}/.angreal/init.py
@@ -1,9 +1,8 @@
 import sys
+import logging
 green = "\33[32m"
 red = "\33[31m"
 end = "\33[0m"
 
 def init():
-    print(green + "=" * 20  + end, file=sys.stderr )
-    print(green + "INITIALIZING OUR PROJECT!!" + end, file=sys.stderr)
-    print(green + "=" * 20  + end , file=sys.stderr)
+    logging.info("INITIALIZING OUR PROJECT!!! - MESSAGE COMES FROM init.py")
